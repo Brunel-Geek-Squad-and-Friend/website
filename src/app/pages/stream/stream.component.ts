d72b559458a3dd6593521a6652db2fb511fab8fe
Aaron Osher <aaron@oshers.com>
2018-02-24 23:44:41 +0000

    Queue Finished

--- src/app/pages/stream/stream.component.ts
+++ src/app/pages/stream/stream.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild, AfterViewInit} from '@angular/core';
+import * as RecordRTC from 'recordrtc';
 
 @Component({
   selector: 'app-stream',
@@ -7,9 +8,84 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StreamComponent implements OnInit {
 
+  @ViewChild('video') video: any;
+
+  private stream: MediaStream;
+
+  private recordRTC: any;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  ngAfterViewInit() {
+    // set the initial state of the video
+    const video: HTMLVideoElement = this.video.nativeElement;
+    video.muted = false;
+    video.controls = true;
+    video.autoplay = false;
+  }
+
+  toggleControls() {
+    const video: HTMLVideoElement = this.video.nativeElement;
+    video.muted = !video.muted;
+    video.controls = !video.controls;
+    video.autoplay = !video.autoplay;
+  }
+
+
+  startRecording() {
+    const mediaConstraints: MediaStreamConstraints = {
+      video: {
+        height: 1280,
+        width: 720
+      }, audio: true
+    };
+    navigator.mediaDevices
+      .getUserMedia(mediaConstraints)
+      .then(this.successCallback.bind(this), this.errorCallback.bind(this));
+  }
+
+  successCallback(stream: MediaStream) {
+    const options = {
+      mimeType: 'video/webm;codes=h264', // or video/webm\;codecs=h264 or video/webm\;codecs=vp9
+      audioBitsPerSecond: 0,
+      videoBitsPerSecond: 128000,
+      bitsPerSecond: 128000 // if this line is provided, skip above two
+    };
+    this.stream = stream;
+    this.recordRTC = RecordRTC(stream, options);
+    this.recordRTC.startRecording();
+    const video: HTMLVideoElement = this.video.nativeElement;
+    video.src = window.URL.createObjectURL(stream);
+    this.toggleControls();
+  }
+
+  errorCallback() {
+    // handle error here
+  }
+
+
+  stopRecording() {
+    const recordRTC = this.recordRTC;
+    recordRTC.stopRecording(this.processVideo.bind(this));
+    const stream = this.stream;
+    stream.getAudioTracks().forEach(track => track.stop());
+    stream.getVideoTracks().forEach(track => track.stop());
+  }
+
+  processVideo(audioVideoWebMURL) {
+    const video: HTMLVideoElement = this.video.nativeElement;
+    const recordRTC = this.recordRTC;
+    video.src = audioVideoWebMURL;
+    this.toggleControls();
+    const recordedBlob = recordRTC.getBlob();
+    recordRTC.getDataURL(function (dataURL) { });
+  }
+
+  download() {
+    this.recordRTC.save('video.webm');
   }
 
 }
