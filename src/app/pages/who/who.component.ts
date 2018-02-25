a5a04baf35978f16fdd4c67d9a03088eaad416b9
Aaron Osher <aaron@oshers.com>
2018-02-25 08:36:23 +0000

    Add Who

--- src/app/pages/who/who.component.ts
+++ src/app/pages/who/who.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {QueueService} from '../../services/queue.service';
+import {map, tap} from 'rxjs/operators';
+import {Observable} from 'rxjs/Observable';
 
 @Component({
   selector: 'app-who',
@@ -7,9 +10,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WhoComponent implements OnInit {
 
-  constructor() { }
+  queue$: Observable<any>;
+
+  constructor(
+    private queueService: QueueService,
+  ) { }
 
   ngOnInit() {
+    this.retrieveQueue();
+  }
+
+  private retrieveQueue() {
+    this.queue$ = this.queueService.get()
+      .pipe(
+        map(this.orderQueue)
+      );
+  }
+
+  private orderQueue(queue: any) {
+    return queue.sort((a, b) => {
+      const dateA = new Date(a.joined), dateB = new Date(b.joined);
+      return dateA > dateB;
+    });
   }
 
 }
